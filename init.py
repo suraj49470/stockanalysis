e526d19ed5e7fe0f1348f70f3b92c969d4e6d552
Suraj Singh <suraj49470@gmail.com>
2023-02-26 12:41:55 +0530

    solvency done

--- init.py
+++ init.py
@@ -3,37 +3,51 @@ from utility import Utility
 from horizontal import Horizontal
 from vertical import Vertical
 from trend import Trend
+from solvency import Solvency
 import time
 
+def init_reports(company) :
+    va = Vertical(company)
+    va.set_va_income_df()
+    print(va.get_va_income_df())
+
+    va.set_va_balance_df()
+    print(va.get_va_balance_df())
+
+    ha = Horizontal(company)
+    ha.set_ha_income()
+    print(ha.get_ha_income())
+
+    ha.set_ha_balance()
+    print(ha.get_ha_balance())
+
+    ha.set_ha_cash()
+    print(ha.get_ha_cash())
 
 
-start_time = time.time()       
-va = Vertical('mastek')
-va.set_va_income_df()
-print(va.get_va_income_df())
+    tr = Trend(company)
+    tr.set_trend_income()
+    print(tr.get_trend_income())
 
-va.set_va_balance_df()
-print(va.get_va_balance_df())
-
-ha = Horizontal('mastek')
-ha.set_ha_income()
-print(ha.get_ha_income())
-
-ha.set_ha_balance()
-print(ha.get_ha_balance())
-
-ha.set_ha_cash()
-print(ha.get_ha_cash())
+    tr.set_trend_balance()
+    print(tr.get_trend_balance())
 
 
-tr = Trend('mastek')
-tr.set_trend_income()
-print(tr.get_trend_income())
+    tr.set_trend_cash()
+    print(tr.get_trend_cash())
 
-tr.set_trend_balance()
-print(tr.get_trend_balance())
+    sl = Solvency(company)
+    sl.set_current_ratio()
+    sl.set_quick_ratio()
+    sl.set_cash_ratio()
+    print(sl.get_solvency())
 
 
-tr.set_trend_cash()
-print(tr.get_trend_cash())
+
+
+start_time = time.time()
+init_reports('mastek')
+
+      
+
 print("--- %s seconds ---" % (time.time() - start_time))
